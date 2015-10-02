ad82d23d2ca88d9a8e2ac73a7b3d213e957f16fb
Willi64 <Willi44_2@gmail.de>
2015-10-02 21:23:34 +0200

    fly even more faster

--- hero/src/Fly.java
+++ hero/src/Fly.java
@@ -7,5 +7,6 @@ public class Fly {
     public static void main(String[] args) {
         System.out.println("Fly!");
         System.out.println("Fly faster!");
+        System.out.println("Fly even more faster!");
     }
 }
